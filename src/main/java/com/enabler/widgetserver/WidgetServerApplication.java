e7d4501558c51a7e954e7bfb1cac6f200d750f59
Waris Quamer <warisqr007@MacBook-Air.local>
2020-08-31 22:57:51 +0530

    Basic skeleton

---
+++ src/main/java/com/enabler/widgetserver/WidgetServerApplication.java
@@ -0,0 +1,13 @@
+package com.enabler.widgetserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WidgetServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WidgetServerApplication.class, args);
+	}
+
+}
